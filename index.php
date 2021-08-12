0eb5bcbf7b1d2f15b528972c28cb7b6759e2e19c
Rahmad Subagiyo <subag.dev@gmail.com>
2021-08-12 15:12:52 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Akses Anda Dilarang</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet'>
+</head>
+<body>
+    <div>
+        <div class="image">
+            <img src="stop-sign.png" alt="">
+        </div>
+        <div class="text">
+            <h1>Oops!!!</h1>
+            <p>Sorry, but you dont have access to <br> this page or source</p>
+            <button class="button">Back to Home</button>
+            <br>
+            <table>
+                <tr>
+                    <td><img class="img1" src="logo-kominfo-copy.png" alt=""></td>
+                    <td><img class="img2" src="logo-fa.png" alt=""></td>
+                    <td><img class="img3" src="mamura.png" alt=""></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</body>
+</html>
